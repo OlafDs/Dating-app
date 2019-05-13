c30051d6f711d690893b489ec8f8508376ae06b0
Olaffff <43337651+Olaffff@users.noreply.github.com>
2019-05-13 20:31:37 +0200

    Commit with new changes. View & Static map

---
+++ sum.js
@@ -0,0 +1,16 @@
+// sum.js
+module.exports = sum
+
+function sum() {
+  var args = arguments
+  var total = 0
+  var index = -1
+
+
+  while (++index < args.length) {
+    total += args[index]
+  }
+
+
+  return total
+}
