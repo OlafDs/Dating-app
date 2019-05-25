71b9a4a4a27ddd7b6acc3998e5ed8780caad907e
OlafDs <scheet16@hotmail.com>
2019-05-25 14:30:09 +0200

    Changed the code a little bit on server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({
 // Set Static path
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use(function(req, res, next){
+app.use(function (req, res, next) {
   res.locals.errors = null
   next();
 });
@@ -50,6 +50,8 @@ app.get("/index", home)
 app.get("/register", register)
 app.get("/login", inloggen)
 app.get("/explore", explore)
+app.get("/profile", profile)
+app.get("/matches", matches)
 
 
 app.post('/', function (req, res) {
@@ -63,46 +65,47 @@ function start(req, res) {
 }
 
 function register(req, res) {
+  db.users.find(function (err, docs) {
   res.render('pages/register.ejs', {
     title: "register",
-    users: users
+    users: docs
   });
+})
 }
 app.post('/users/add', function (req, res) {
 
-  req.checkBody('name', 'Voornaam is verplicht').notEmpty;
-  req.checkBody('email', 'Email is verplicht').notEmpty;
+  req.checkBody('first_name', 'Voornaam is verplicht').notEmpty;
+  req.checkBody('last_name', 'Achternaam is verplicht').notEmpty;
+  req.checkBody('leeftijd', 'Leeftijd is verplicht').notEmpty;
+  req.checkBody('sport', 'Sport is verplicht').notEmpty;
   req.checkBody('description', 'Info over jezelf is verplicht').notEmpty;
 
 
-  var errors = req.validationErrors();
-
-  if (errors) {
-    // console.log('error');
-    res.render('pages/register.ejs', {
-      title: "register",
-      users: users,
-      errors: errors
-    });
-  } else {
-    var newUser = {
-      first_name: req.body.firstname,
-      email: req.body.email,
-      description: req.body.description
+  console.log('Registeren is gelukt');
+  res.redirect("../userprofile");
+  db.users.insert(newUser, function (req, res) {
+    if (err) {
+      console.log(err);
     }
+  });
+})
+app.delete('/users/delete', function (req, res) {
 
-    console.log('Registeren is gelukt');
-    // db.users.insert(newUSer, (errors, req, res)
-      // studenten_nummer: req.body.studenten_nummer,
-      // password: req.body.password
+
+  console.log('Registeren is gelukt');
+  res.redirect("../userprofile");
+  db.users.remove(docs, function (reg, res) {
+    if (err) {
+      console.log(err);
     }
+  });
+})
 
-    console.log('Registeren is gelukt');
-  }
-
-
-);
-
+function inloggen(req, res) {
+  res.render('pages/inloggen.ejs', {
+    title: "inloggen"
+  });
+}
 
 function login(req, res) {
   res.render('pages/Login.ejs', {
@@ -129,14 +132,14 @@ function profile(req, res) {
 }
 
 function userprofile(req, res) {
-  db.users.find(function(err, docs){
+  db.users.find(function (err, docs) {
     console.log(docs);
     res.render('pages/userprofile.ejs', {
       title: "userprofile",
       users: docs
     });
-  }) 
-  
+  })
+
 }
 
 app.use(function (req, res, next) {
